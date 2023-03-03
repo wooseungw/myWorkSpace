3761207ef9222e60f78c00d740cc426123a55898
wooseungw <woow070840@gmail.com>
2023-03-03 18:17:51 +0900

    add 2798

---
+++ Algorithm/Baekjoon/baekjun 2798.py
@@ -0,0 +1,40 @@
+n, m = input().split(" ")
+list_input = input().split(" ")
+list = list(map(int, list_input))
+list.sort(reverse=True)
+result_list = []
+a = len(result_list)
+
+for i in list:
+    result_list.append(i)
+    list.remove(i)
+    for j in list:
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range(len(list)+ 1):
+#     if a ==3:
+#         break
+#     if sum(result_list) < int(m):
+        
+#         if sum(result_list) + list[i] < int(m):
+#             result_list.append(list[i])
+#             print(result_list)
+#         else:
+#             print(result_list)
+            
+    
+        
+    
